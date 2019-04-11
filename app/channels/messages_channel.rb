001d789a522cb4eeb492c2c19d704d66a7fdbdc9
Roberto <=>
2019-04-10 22:55:24 -0300

    Set channel files.

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,10 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    conversation = Conversation.find(params[:conversation])
+    stream_from conversation
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  
   end
 end
