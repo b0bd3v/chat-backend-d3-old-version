61c86b81bfaecd64b628f158edb1f37bcb09ba6c
Roberto <=>
2019-04-10 23:10:21 -0300

    Actions index and create was added into Conversation Controller.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,26 @@
 class ConversationsController < ApplicationController
+    
+    # List of conversations.
+    def index
+        conversations = Conversation.all
+        render json: conversations
+    end
+
+    # Action to create a conversation.
+    def create
+        conversation = Conversation.new(conversation_params)
+        if conversation.save
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(
+                ConversationSerializer.new(conversation)
+            ).serialized_hash
+            ActionCable.server.broadcast 'conversation_channel', serialized_data
+            head :ok
+        end
+    end
+
+    private
+
+    def conversation_params
+        params.require(:conversation).permit(:title)
+    end
 end
